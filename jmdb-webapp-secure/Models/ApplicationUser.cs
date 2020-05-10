3732a9368bb7d50213495341c88b737dd5bf76f1
jitups <jitusurve@gmail.com>
2020-05-11 00:59:21 +0530

    added web app with login page

---
+++ jmdb-webapp-secure/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jmdb_webapp_secure.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
