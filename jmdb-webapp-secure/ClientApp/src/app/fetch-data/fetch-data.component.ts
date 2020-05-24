d3a4ef325f41939f1b7d4604d933049bc7011353
jitups <jitusurve@gmail.com>
2020-05-24 17:27:02 +0530

    added model reference to add movie

--- jmdb-webapp-secure/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ jmdb-webapp-secure/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -2,13 +2,14 @@ import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MovieServiceService } from '../services/movie-service.service';
 import { Subscription } from 'rxjs';
+import { MovieModel } from '../models/movie-model';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent implements OnInit,OnDestroy {
-  public movies: Array<any> = [];
+  public movies: Array<MovieModel> = [];
   private subscriptions: Array<Subscription>=[];
 
   constructor(http: HttpClient,
@@ -35,7 +36,7 @@ export class FetchDataComponent implements OnInit,OnDestroy {
     });
   }
 
-  displayRow(movie:any){
+  displayRow(movie:MovieModel){
     this.movieService.movieSelected$.next(movie);
   }
 }
